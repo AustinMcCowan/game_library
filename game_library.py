d01ba61a2519eb6897f6a1729fdac69099ad6c3b
AustinMcCowan <amccowan@stcharlessd.org>
2020-01-28 13:17:43 -0600

    Added pickle file and reboot file, added printing and saving

--- game_library.py
+++ game_library.py
@@ -4,57 +4,78 @@
 
 ''' Game Library program. Borrow content from big_brother.py. Will later turn into GUI, for now it is console based. '''
 
-'''
+import pickle
+import data_reboot as dr
+
 class Library(object):
     def __init__(self):
-        try:
-            
-            PLACEHOLDER = open("PLACEHOLDER.pickle", "rb")
-            self.games = pickle.load(PLACEHOLDER)
-            PLACEHOLDER.close()
-            
-        except:
-            
-            raise Exception("Data failed to load")
-            
         
-    # updates the information in PLACEHOLDER.pickle with the current information in the program (games)
-    def update(self):
-        try:
+        # Try and open the pickle file and grab contents
+        try: 
+            datafile = open("game_lib.pickle", "rb")
+            self.games = pickle.load(datafile)
             
-            PLACEHOLDER = open("PLACEHOLDER.pickle", "wb")
-            pickle.dump(self.games, PLACEHOLDER)
-            PLACEHOLDER.close()
+        # If unable to grab content, run the function inside of data_reboot.py. Then try loading content again.    
+        except:            
+            dr.fix_it()
+            print("Data failed to load; Grabbing original content")
+            datafile = open("game_lib.pickle", "rb")
+            self.games = pickle.load(datafile)
+        
+        # Close datafile    
+        datafile.close()
+            
+    # updates the information in game_lib.pickle with the current information in the program (games)
+    def save(self):
+        try:
+            datafile = open("game_lib.pickle", "wb")
+            pickle.dump(self.games, datafile)
+            datafile.close()
             
         except:
             
-            raise Exception("Data failed to update")     
-'''
-# First entry to plug in.
-#games = {1: ['RPG', 'Nier:Automa', 'PlatinumGames', 'Square Enix', 'PS4', '3/17/2017', '10', 'single', '39.99', 'Yes', '1/15/2018', 'Become as gods.']}
+            raise Exception("Data failed to save")     
+
 
 def add_or_edit_game():
-    print("adding or editing game...")
+    print("\nAdding or editing game...\n")
 
 def print_info():
-    print("printing info...")
+    for game in content.games:
+        print("----------------------------")
+        print("Genre:", content.games[game][0])
+        print("Title:", content.games[game][1])
+        print("Developer:", content.games[game][2])
+        print("Publisher:", content.games[game][3])
+        print("System:", content.games[game][4])
+        print("Release Date:", content.games[game][5])
+        print("Rating:", content.games[game][6])
+        print("Single/Multi/Either:", content.games[game][7])
+        print("Price:", content.games[game][8])
+        print("Beat it?:", content.games[game][9])
+        print("Purchase Date:", content.games[game][10])
+        print("Notes:", content.games[game][11])
+        print("----------------------------")
     
 def search_by_title():
-    print("searching by title...")
+    print("\nSearching by title...\n")
     
 def remove_game():
-    print("removing a game...")
+    print("\nRemoving a game...\n")
 
 def save_database():
-    print("saving database...")
+    print("\nSaving Changes...\n")
+    content.save()
     
 def quit_app():
-    print("quitting")
+    print("\nQuitting...\n")
     
-# content = Library()   
 
+# Initial stuff that needs to be added before main program    
+content = Library()   
 quit = False
 
+# Main
 while quit != True: 
     
     print('''Choosable actions:
@@ -71,8 +92,7 @@ while quit != True:
         add_or_edit_game()
     
     elif user_command == "2":
-        for i in range(3):
-            print_info()
+        print_info()
             
     elif user_command == "3":       
         search_by_title()
@@ -83,7 +103,6 @@ while quit != True:
     elif user_command == "5":
         save_database()
         
-    elif user_command == "q":
+    elif user_command.lower() == "q":
         quit_app()
         quit = True
-        
